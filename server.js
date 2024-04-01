bd825c2b581babe1ed61679fffd5813ae369a585
Jonathan Tan <jtsz5057@gmail.com>
2024-04-01 23:13:27 +1000

    create route structure

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require('express');
+const dotenv = require('dotenv');
 
 //Load env vars
 dotenv.config({ path: './config/.env' })
 
 const app = express();
+
+// GET /api/v1/bootcamps
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Show all Bootcamps' })
+});
+
+// POST /api/v1/bootcamps
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Create new Bootcamps' })
+});
+
+// PUT /api/v1/bootcamps
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Update bootcamps ${req.params.id}` })
+});
+
+// DELETE /api/v1/bootcamps
+app.delete('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ success: true, msg: `Delete bootcamps ${req.params.id}` })
+});
 
 const PORT = process.env.PORT || 5000
 

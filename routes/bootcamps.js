b463c56760667185f8ac1fedb4af34d4db1879ac
Jonathan Tan <jtsz5057@gmail.com>
2024-04-01 23:55:19 +1000

    mount all the routers

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -2,26 +2,28 @@ const express = require('express');
 const router = express.Router()
 
 // GET /api/v1/bootcamps
-router.get('/api/v1/bootcamps', (req, res) => {
+router.get('/', (req, res) => {
     res.status(200).json({ success: true, msg: 'Show all Bootcamps' })
 });
 
 // GET /api/v1/bootcamps/:id
-router.get('/api/v1/bootcamps/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     res.status(200).json({ success: true, msg: `Show bootcamps ${req.params.id}` })
 });
 
 // POST /api/v1/bootcamps
-router.post('/api/v1/bootcamps', (req, res) => {
+router.post('/', (req, res) => {
     res.status(200).json({ success: true, msg: 'Create new Bootcamps' })
 });
 
 // PUT /api/v1/bootcamps
-router.put('/api/v1/bootcamps/:id', (req, res) => {
+router.put('/:id', (req, res) => {
     res.status(200).json({ success: true, msg: `Update bootcamps ${req.params.id}` })
 });
 
 // DELETE /api/v1/bootcamps
-router.delete('/api/v1/bootcamps', (req, res) => {
+router.delete('/:id', (req, res) => {
     res.status(200).json({ success: true, msg: `Delete bootcamps ${req.params.id}` })
 });
+
+module.exports = router; 
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
-//Load env vars
+// Route files
+const bootcamps = require('./routes/bootcamps')
+
+// Load env vars
 dotenv.config({ path: './config/.env' })
 
 const app = express();
+
+//  Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 

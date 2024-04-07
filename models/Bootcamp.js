719b6e1d500a2b7a3a060525fe34acef135475ef
Jonathan Tan <jtsz5057@gmail.com>
2024-04-07 23:03:23 +1000

    add website field to bootcamp Schema

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -13,7 +13,14 @@ const BootcampSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please add a description'],
         maxlength: [500, 'Description can not be more than 500 characrers']
-    }
+    },
+    Website: {
+        type: String,
+        match: [
+            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+            'Please use a valid URL with HTTP or HTTOS'
+        ]
+    },
 });
 
 module.exports = mongoose.model

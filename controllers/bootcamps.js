8da553d82fef822a14f4a8eff6abebe0b41dcc0f
Jonathan Tan <jtsz5057@gmail.com>
2024-04-12 21:36:06 +1000

    create Post method for bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -4,6 +4,7 @@ const Bootcamp = require('../models/Bootcamp')
 // @route   Get /api/v1/bootcamps
 // @access  Public 
 exports.getBootcamps = (req, res, next) => {
+    console.log(req.body)
     res.status(200).json({ success: true, msg: 'Show all Bootcamps' });
 }
 
@@ -17,9 +18,16 @@ exports.getBootcamp = (req, res, next) => {
 // @desc    Create bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private 
-exports.createBootcamp = (req, res, next) => {
-    console.log(req.body)
-    res.status(200).json({ success: true, msg: 'Create new Bootcamps' })
+exports.createBootcamp = async (req, res, next) => {
+
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        data: bootcamp
+    })
+    // console.log(req.body)
+    // res.status(200).json({ success: true, msg: 'Create new Bootcamps' })
 }
 
 // @desc    Update bootcamp

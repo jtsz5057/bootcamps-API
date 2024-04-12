f070b7f6667f58b419a5c632243b4eb89e3cad13
Jonathan Tan <jtsz5057@gmail.com>
2024-04-12 21:43:58 +1000

    refactor post method with try catch

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -19,13 +19,19 @@ exports.getBootcamp = (req, res, next) => {
 // @route   POST /api/v1/bootcamps
 // @access  Private 
 exports.createBootcamp = async (req, res, next) => {
+    try {
+         const bootcamp = await Bootcamp.create(req.body);
 
-    const bootcamp = await Bootcamp.create(req.body);
-
-    res.status(201).json({
-        success: true,
-        data: bootcamp
-    })
+        res.status(201).json({
+            success: true,
+            data: bootcamp
+        });
+    } catch (err) {
+        res.status(400).json({
+            success: false
+        })
+    }
+    
     // console.log(req.body)
     // res.status(200).json({ success: true, msg: 'Create new Bootcamps' })
 }

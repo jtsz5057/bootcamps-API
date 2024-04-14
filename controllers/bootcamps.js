e6481fea5daae8dfab9c361ab49e7bbbb5a44927
Jonathan Tan <jtsz5057@gmail.com>
2024-04-14 23:45:49 +1000

    add try catch error on update request method

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -67,21 +67,28 @@ exports.createBootcamp = async (req, res, next) => {
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private 
 exports.updateBootcamp = async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    });
-
-    if(!bootcamp) {
-        return res.status(400).json({
-            success: false
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+    
+        if(!bootcamp) {
+            return res.status(400).json({
+                success: false
+            })
+        }
+    
+        res.status(200).json({
+            success: true,
+            data: bootcamp
         })
-    }
+    } catch (err) { 
+        res.status(400).json({
+            success: false
+        });
 
-    res.status(200).json({
-        success: true,
-        data: bootcamp
-    })
+    };
 }
 
 // @desc    Delete bootcamp

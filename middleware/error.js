c3c586eca02feda6897120b727064364b2937c22
Jonathan Tan <jtsz5057@gmail.com>
2024-04-19 18:07:29 +1000

    fix: fix bug in the error handler

--- middleware/error.js
+++ middleware/error.js
@@ -9,7 +9,7 @@ const errorHandler = (err, req, res, next) => {
     console.log(err.stack.red);
 
     // Mongoose bad object id
-    if (err.name === 'castError') {
+    if (err.name === 'CastError') {
         const message = `Resource not found with the id of ${err.value}`
         error = new ErrorResponse(message, 404);
     }
